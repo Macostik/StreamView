948f66a20e06355a1094058491901a152bdf262d
Macostik <y.granchenko@simcord.com>
2018-09-26 17:30:51 +0300

    add willBeginDragging

--- Source/StreamDataSource.swift
+++ Source/StreamDataSource.swift
@@ -108,6 +108,11 @@ open class StreamDataSource<T: BaseOrderedContainer>: NSObject, StreamViewDataSo
     }
     
     open var didEndDecelerating: (() -> ())?
+    open var willBeginScrolling: (() -> ())?
+    
+    open func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        willBeginScrolling?()
+    }
     
     open func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
         if !decelerate {
